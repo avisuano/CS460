95b1dd11cca0284b013b48499e1c1e665c4998fd
Anthony Visuano <avisuano15@wou.edu>
2018-10-21 20:55:06 -0700

    working on fixes for mile converter

--- HW4/Homework4/Homework4/Controllers/HomeController.cs
+++ HW4/Homework4/Homework4/Controllers/HomeController.cs
@@ -8,21 +8,62 @@ namespace Homework4.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult MileConverter()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [ActionName("MileConverter")]
+        public ActionResult MileConverterResult(FormCollection fc)
         {
-            ViewBag.Message = "Your contact page.";
+            double miles = Convert.ToDouble(Request.Form["miles"]);
+
+            if (fc["toconvert"] == "mm")
+            {
+                ViewBag.results = miles * 1609344;
+            }
+            else if (fc["toconvert"] == "cm")
+            {
+                ViewBag.results = miles * 160934;
+            }
+            else if (fc["toconvert"] == "m")
+            {
+                ViewBag.results = miles * 1609.34;
+            }
+            else if (fc["toconvert"] == "km")
+            {
+                ViewBag.results = miles * 1.60934;
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Color()
+        {
+            ViewBag.Message = "Color Choser.";
 
             return View();
         }
