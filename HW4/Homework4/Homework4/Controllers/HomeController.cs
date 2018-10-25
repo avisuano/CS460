f42c9eaa5925673beeaf9e68f0f9513a38481e39
Anthony Visuano <avisuano15@wou.edu>
2018-10-24 17:51:02 -0700

    holy smokes it's working

--- HW4/Homework4/Homework4/Controllers/HomeController.cs
+++ HW4/Homework4/Homework4/Controllers/HomeController.cs
@@ -77,7 +77,7 @@ namespace Homework4.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        public ActionResult Color()
+        public ActionResult Colors()
         {       
             ViewBag.ColorResult = false;
             return View();
@@ -90,27 +90,76 @@ namespace Homework4.Controllers
         /// <param name="SecondColor"></param>
         /// <returns></returns>
         [HttpPost]
-        [ActionName("Color")]
-        public ActionResult Color(string FirstColor, string SecondColor)
+        [ActionName("Colors")]
+        public ActionResult Colors(string FirstColor, string SecondColor)
         {
             ViewBag.ColorResult = false;
 
+            // Grab the hexadecimal values from the user
             FirstColor = Request.Form["FirstColor"];
             SecondColor = Request.Form["SecondColor"];
 
+            // This converts the hexadecimal values into their alpha, red, blue, and green values
             Color color1 = ColorTranslator.FromHtml(FirstColor);
             Color color2 = ColorTranslator.FromHtml(SecondColor);
+
+            // Initialize the values needed for error checking
+            int color_alpha, color_red, color_blue, color_green;
+
+            // This is needed to check for values above 255 -- lots of errors if these aren't checked
+            // Started with alpha...
+            if (color1.A + color2.A >= 255)
+            {
+                color_alpha = 255;
+            }
+            else
+            {
+                color_alpha = color1.A + color2.A;
+            }
+            // Next the red values are checked
+            if (color1.R + color2.R >= 255)
+            {
+                color_red = 255;
+            }
+            else
+            {
+                color_red = color1.R + color2.R;
+            }
+            // Next the blue values are checked
+            if (color1.B + color2.B >= 255)
+            {
+                color_blue = 255;
+            }
+            else
+            {
+                color_blue = color1.B + color2.B;
+            }
+            // Finally the green values are checked
+            if (color1.G + color2.G >= 255)
+            {
+                color_green = 255;
+            }
+            else
+            {
+                color_green = color1.G + color2.G;
+            }
+            // Take the alpha, red, blue, and green values and combine them into ARGB format
+            // After being combined, they are repacked into a HTML format to be viewed
+            string MixedColors = ColorTranslator.ToHtml(Color.FromArgb(color_alpha, color_red, color_blue, color_green));
 
             Debug.WriteLine(FirstColor);
             Debug.WriteLine(SecondColor);
             Debug.WriteLine(color1);
             Debug.WriteLine(color2);
+            Debug.WriteLine(MixedColors);
 
+            // After the values are checked and put together, they will be pushed to the Colors view page
             if (FirstColor != null && SecondColor != null)
             {
                 ViewBag.ColorResult = true;
                 ViewBag.Color1 = "width:50px; height:50px; border: 1px soild #000; background:" + FirstColor;
                 ViewBag.Color2 = "width:50px; height:50px; border: 1px soild #000; background:" + SecondColor;
+                ViewBag.MixedColors = "width:50px; height:50px; border: 1px soild #000; background:" + MixedColors;
             }
 
             return View();
