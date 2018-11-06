4a2d21a5bc0387d23f7c537e9f771a461b7f5ae3
Anthony Visuano <avisuano15@wou.edu>
2018-11-05 23:19:10 -0800

    Almost got the database working

---
+++ HW6/Homework6/DAL/WWIContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Homework6.Models;
+
+namespace Homework6.DAL
+{
+    public class WWIContext : DbContext
+    {
+        public WWIContext() : base("name=WideWorldImportersEntities") { }
+
+        public virtual DbSet<WideWorldImportersEntitie> WideWorldImportersEntities { get; set; }
+    }
+}
