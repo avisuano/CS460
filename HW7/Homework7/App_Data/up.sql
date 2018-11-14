bf499fe809e4f2f3c8f07989b39b394e6d9dd734
Anthony Visuano <avisuano15@wou.edu>
2018-11-13 20:56:01 -0800

    early progress?

---
+++ HW7/Homework7/App_Data/up.sql
@@ -0,0 +1,11 @@
+﻿------------------------------
+-- Table of logged searches --
+------------------------------
+CREATE TABLE [dbo].[SearchLogs]
+(
+	[ID]		INT	IDENTITY (1,1)	NOT NULL,
+	[Search]	NVARCHAR(MAX)		NOT NULL,
+	[SearchIP]	NVARCHAR			NOT NULL,
+	[Date]		DateTime			NOT NULL,
+	CONSTRAINT [PK.dbo.SearchLogs] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
