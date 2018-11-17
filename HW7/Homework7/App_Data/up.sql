4b5d71d68036c6a492e429685ec79c90bf280713
Anthony Visuano <avisuano15@wou.edu>
2018-11-16 19:22:55 -0800

    problems abound

--- HW7/Homework7/App_Data/up.sql
+++ HW7/Homework7/App_Data/up.sql
@@ -9,3 +9,7 @@ CREATE TABLE [dbo].[SearchLogs]
 	[TimeStamp]	DateTime			NOT NULL,
 	CONSTRAINT [PK.dbo.SearchLogs] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+INSERT INTO [dbo].[SearchLogs] (Search,SearchIP,TimeStamp) VALUES
+	('put somthing to display','192.168.1.1','2018-11-15 00:00:00')
+GO
--- HW7/Homework7/Scripts/translate.js
+++ HW7/Homework7/Scripts/translate.js
@@ -1,5 +1,11 @@
-﻿// Add the text as typed to the DOM
-//$("#translate").keyup(function (event) {
-//    var txt = $(this).val();
-//    $("#destination").text(txt);
-//});
+﻿$(document).ready(function () {
+
+    // Add the text as typed to the DOM... for fun...
+    $("#translate").keyup(function (event) {
+        var txt = $(this).val();
+        $("#destination").text(txt);
+    });
+
+
+     
+});
